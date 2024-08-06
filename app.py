042ca72b1706841d94d21f0a6fdd2686f10fc746
sweetofu35 <62172639+sweetofu35@users.noreply.github.com>
2024-08-07 04:47:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ if st.session_state.logged_in: # 로그인 시 다음 페이지로 이동
             x,y = get_coordinates(st.session_state.do, st.session_state.city)
             params["nx"] = x
             params["ny"] = y
-            url = f"http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey={serviceKey}&numOfRows=800&pageNo=1&dataType=json&base_date={base_date}&base_time=2300&nx={x}&ny={y}"
+            url = f"http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey={serviceKey}&numOfRows=800&pageNo=1&dataType=json&base_date={base_date}&base_time=0600&nx={x}&ny={y}"
             response = requests.get(url, verify=False)
             res = json.loads(response.text)
             st.write(res)
