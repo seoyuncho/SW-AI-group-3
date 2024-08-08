9098e2ab5d978ecf9d9f6da2b19025839e271863
sweetofu35 <62172639+sweetofu35@users.noreply.github.com>
2024-08-08 17:00:48 +0900

    Update final.py

--- final.py
+++ final.py
@@ -214,7 +214,10 @@ if st.session_state.logged_in:
         # 선택된 값들을 영어로 변환하여 저장
 
         if st.button("추가"):
-            user_info_optional[st.session_state.username].append([st.session_state.cloths, st.session_state.types, st.session_state.material, st.session_state.color])
+            if st.session_state.username not in user_info_optional.keys():
+                user_info_optional[st.session_state.username] = ([st.session_state.cloths, st.session_state.types, st.session_state.material, st.session_state.color])
+            else:
+                user_info_optional[st.session_state.username].append([st.session_state.cloths, st.session_state.types, st.session_state.material, st.session_state.color])
             new_text = str(user_info_optional)
             with open('./user_info_optional.txt', 'w', encoding='UTF-8') as f:
                 f.write(new_text)
