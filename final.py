148d4b2e797e1e1836cd39daad031b12b1ae68f8
sweetofu35 <62172639+sweetofu35@users.noreply.github.com>
2024-08-08 16:52:16 +0900

    Update final.py

--- final.py
+++ final.py
@@ -147,7 +147,6 @@ if st.session_state.logged_in:
                 new_text = str(user_info)
                 with open('./user_info.txt','w',encoding='UTF-8') as f:
                     f.write(new_text)
-                user_is_first[st.session_state.username] = False
                 new_text = str(user_is_first)
                 with open('./user_is_first.txt','w',encoding='UTF-8') as f:
                     f.write(new_text)
@@ -158,6 +157,7 @@ if st.session_state.logged_in:
                 st.rerun()
 
         if st.session_state.page == 2: # 옷 정보 입력 선택 페이지
+            user_is_first[st.session_state.username] = False
             st.write("(선택) 가지고 있는 옷 정보를 입력하시겠습니까? (나중에 언제든지 다시 입력할 수 있습니다.)")
             if st.button("예"):
                 st.session_state.add_cloths = True
